8d0df1f8b2abf3515f1054e38ca93b63507a09e4
Alvaro Dasso <alvarodasso@Alvaros-MacBook-Pro.local>
2017-05-27 10:19:04 -0500

    Initial Commit

---
+++ computerRegister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  computerRegister
+//
+//  Created by Alvaro Dasso on 5/27/17.
+//  Copyright © 2017 Alvaro Dasso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
